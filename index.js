c86e1eeb22f294dedb9f933a5da0fd64b29e9cb2
Vitor <vitorgpalves@gmail.com>
2019-10-20 02:06:48 -0300

    feature: cria middlewares global e locais

--- index.js
+++ index.js
@@ -4,33 +4,60 @@ const server = express()
 
 server.use(express.json())
 
+server.use((request, response, next) => {
+  console.log(`Method: ${request.method}, URL: ${request.url}`)
+  next()
+})
+
 const users = ["Vitor", "Diego", "Lucas"]
+
+const checkUserNameExistsInRequest = (request, response, next) => {
+  if (!request.body.name) {
+    return response.status(400).json({ error: "User name is required." })
+  }
+
+  next()
+}
+
+const checkUserInArray = (request, response, next) => {
+  const { index } = request.params
+  if (!users[index]) {
+    return response.status(400).json({ error: "User doesn't exists." })
+  }
+
+  next()
+}
 
 server.get("/users", (request, response) => {
   response.json(users)
 })
 
-server.get("/users/:index", (request, response) => {
+server.get("/users/:index", checkUserInArray, (request, response) => {
   const { index } = request.params
   response.json({ name: users[index] })
 })
 
-server.post("/users", (request, response) => {
+server.post("/users", checkUserNameExistsInRequest, (request, response) => {
   const { name } = request.body
   users.push(name)
   return response.json(users)
 })
 
-server.put("/users/:index", (request, response) => {
-  const { index } = request.params
-  const { name } = request.body
+server.put(
+  "/users/:index",
+  checkUserInArray,
+  checkUserNameExistsInRequest,
+  (request, response) => {
+    const { index } = request.params
+    const { name } = request.body
 
-  users[index] = name
+    users[index] = name
 
-  return response.json(users[index])
-})
+    return response.json(users[index])
+  }
+)
 
-server.delete("/users/:index", (request, response) => {
+server.delete("/users/:index", checkUserInArray, (request, response) => {
   const { index } = request.params
   users.splice(index, 1)
 
